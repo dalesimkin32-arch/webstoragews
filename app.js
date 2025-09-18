69fa063cf3e5bcedd294e8c1bcc8526c34d8fc1c
Dale Simkin <dale.simkin32@gmail.com>
2025-09-18 10:20:01 +0100

    weand local storage ws

--- app.js
+++ app.js
@@ -15,3 +15,75 @@ if (colour) {
   const input = document.querySelector("input");
   input.value = colour;
 }
+
+function savePreferences(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const colour = formData.get("colour");
+
+  // preferences is now an object, it might contain other preferences...
+  const preferences = {
+    colour,
+  };
+  // so when we save it, we stringify it
+  localStorage.setItem("preferences", JSON.stringify(preferences));
+}
+
+form.addEventListener("submit", savePreferences);
+
+// then we parse the string back into an object when we retrieve it
+const preferences = JSON.parse(localStorage.getItem("preferences"));
+
+if (preferences) {
+  const input = document.querySelector("input");
+  input.value = preferences.colour;
+}
+
+const body = document.querySelector("body");
+
+function loadPreferences() {
+  // load the prefs
+  const preferences = JSON.parse(localStorage.getItem("preferences"));
+
+  // if we have some prefs
+  if (preferences) {
+    // set the form value
+    const input = document.querySelector("input");
+    input.value = preferences.colour || "#000000"; // || is the "or" operator, it will use the value on the left if it's truthy, otherwise it will use the value on the right as a default value if colour isn't saved
+
+    // set the body color to the user colour preference (intentional US / UK spelling difference...)
+    /* const body = document.querySelector("body"); */
+    body.style.color = preferences.colour || "#000000";
+  }
+}
+
+function clearPreferences(event) {
+  event.preventDefault();
+
+  localStorage.removeItem("preferences");
+}
+
+const defaults = {
+  colour: "#000000",
+};
+
+body.style.color = preferences.colour || defaults.colour;
+
+const butReset = document.getElementById("resetToDefaultPreferences");
+
+butReset.addEventListener("click", function () {
+  body.style.color = defaults.colour;
+});
+
+// Function to handle changes in local storage
+function handleLocalStorageChange(event) {
+  if (event.key === "favouriteColour") {
+    // Do something when the specific local storage key changes
+    const newValue = event.newValue;
+    console.log(`Local storage favouriteColour changed to: ${newValue}`);
+  }
+}
+
+// Add an event listener to listen for changes in local storage
+window.addEventListener("storage", handleLocalStorageChange);
